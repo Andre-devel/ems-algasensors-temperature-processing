abe98ef11ab2a29791a5c77dd0987ed9cc5fd74a
Andre goncalves <andrevilanovagon@hotmail.com>
2025-04-28 22:47:19 -0300

    new project files

---
+++ src/test/java/com/andredevel/algasensors/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andredevel.algasensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
