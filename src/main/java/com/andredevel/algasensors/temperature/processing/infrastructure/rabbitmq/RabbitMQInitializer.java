2cdb8f1ed116f16170795e5d21aafa73299f0620
Andre goncalves <andrevilanovagon@hotmail.com>
2025-05-03 13:04:57 -0300

    feat: add and configure RabbitMQ

---
+++ src/main/java/com/andredevel/algasensors/temperature/processing/infrastructure/rabbitmq/RabbitMQInitializer.java
@@ -0,0 +1,22 @@
+package com.andredevel.algasensors.temperature.processing.infrastructure.rabbitmq;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQInitializer {
+    
+    private final RabbitAdmin rabbitAdmin;
+    
+    /**
+     * Método que inicializa o RabbitAdmin, criando as exchanges e queues
+     */
+    @PostConstruct
+    public void init() {
+        // Cria as exchanges e queues
+        rabbitAdmin.initialize();
+    }
+}
