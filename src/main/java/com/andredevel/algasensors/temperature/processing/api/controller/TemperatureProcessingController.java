b3ac793c26e617bef29c6e5fa5ce3d70cdfa2e8d
Andre goncalves <andrevilanovagon@hotmail.com>
2025-05-03 13:42:04 -0300

    feat: add message sending with RabbitMQ and configure TSID serialization

--- src/main/java/com/andredevel/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/andredevel/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -2,10 +2,13 @@ package com.andredevel.algasensors.temperature.processing.api.controller;
 
 import com.andredevel.algasensors.temperature.processing.api.model.TemperatureLogOutput;
 import com.andredevel.algasensors.temperature.processing.common.IdGenerator;
+import static com.andredevel.algasensors.temperature.processing.infrastructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
 import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,14 +22,18 @@ import java.time.OffsetDateTime;
 @RestController
 @RequestMapping("api/sensors/{sensorId}/temperatures/data")
 @Slf4j
+@RequiredArgsConstructor
 public class TemperatureProcessingController {
+    
+    private final RabbitTemplate rabbitTemplate;
+    
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public void data(@PathVariable TSID sensorId, @RequestBody String input) {
         if (input == null || input.isBlank()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
         
-        double temperature;
+        Double temperature;
         
         try {
             temperature = Double.parseDouble(input);
@@ -42,6 +49,15 @@ public class TemperatureProcessingController {
                 .build();
 
         log.info(logOutput.toString());
+        
+        // Cria um MessagePostProcessor para adicionar o header "sensorId" à mensagem
+        MessagePostProcessor messagePostProcessor = message -> {
+            message.getMessageProperties().setHeader("sensorId", logOutput.getSensorId().toString());
+            return message;
+        };
+        
+        // Envia a mensagem para a exchange 
+        rabbitTemplate.convertAndSend(FANOUT_EXCHANGE_NAME, logOutput, messagePostProcessor);
         
     } 
 }
--- src/main/java/com/andredevel/algasensors/temperature/processing/infrastructure/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/andredevel/algasensors/temperature/processing/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -1,14 +1,24 @@
 package com.andredevel.algasensors.temperature.processing.infrastructure.rabbitmq;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    
+    public static final String FANOUT_EXCHANGE_NAME = "temperature-processing.temperature-received.v1.e";
+    
+    // Se passar o ObjectMapper como parâmetro, o Spring irá injetar o bean já configurado (Rest api)
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
     
     // Necessário para criação exchange e queues
     @Bean
@@ -19,7 +29,7 @@ public class RabbitMQConfig {
     // Configuração da exchange
     @Bean
     public FanoutExchange exchange() {
-        return ExchangeBuilder.fanoutExchange("temperature-processing.temperature-received.v1.e").build();
+        return ExchangeBuilder.fanoutExchange(FANOUT_EXCHANGE_NAME).build();
     }
     
     
